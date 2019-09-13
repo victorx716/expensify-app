294906c4c9cf1cc2648d2f349d475ce071332b58
Victor Liu <vliu@ibm.com>
2019-09-13 12:36:20 -0500

    Sort redux data

--- react-redux/src/playground/redux-expensify.js
+++ react-redux/src/playground/redux-expensify.js
@@ -21,12 +21,36 @@ const removeExpense = ({ id } = {}) => ({
   id
 });
 
-// EDIT_EXPENSE
-// SET_TEXT_FILTER
-// SORT_BY_DATE
-// SORT_BY_AMOUNT
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
+
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+})
+
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+})
+
 // SET-START-DATE
+const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate
+})
+
 // SET-END-DATE
+const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate
+})
 
 // Expenses reducer
 const expensesReducerDefaultState = [];
@@ -40,6 +64,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       ];
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id );
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        }
+      })
     default:
       return state;
   }
@@ -55,13 +90,56 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }   
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      }
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      }
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      }
     default:
       return state;
   }
 }
 
-// Create store
+// jan 1 1970 (unix epoch)
 
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1: -1;
+    } else if (sortBy === 'amount') {
+      return a.amount > b.amount ? 1: -1
+    }
+  });
+}
+
+// Create store
 const store = createStore(
   combineReducers({
     expenses: expensesReducer,
@@ -70,17 +148,30 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
-const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 250}));
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: -2000}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 250, createdAt: -1000}));
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }))
-console.log(expenseOne)
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 
-console.log(store.getState());
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
+// store.dispatch(setTextFilter('rent'))
+
+store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+// console.log(expenseOne)
+
+// console.log(store.getState());
+
+// store.dispatch(setStartDate(0)); 
+// store.dispatch(setStartDate()); // start date undefined
+// store.dispatch(setEndDate(999));
 
 // const demoState = {
 //   expenses: [{
@@ -98,12 +189,15 @@ console.log(store.getState());
 //   }
 // }
 
-const user = {
-  name: 'Jen',
-  age: 24
-};
+// const user = {
+//   name: 'Jen',
+//   age: 24
+// };
 
 // how to get new user without changing user object
-console.log(user)
+// console.log(user)
 
-console.log({...user})
+// console.log({
+//   ...user,
+//   age : 18
+// })
