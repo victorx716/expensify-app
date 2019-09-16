677dae84baff5511fd7305e4c0330bda3d2d9064
Victor Liu <vliu@ibm.com>
2019-09-16 13:19:29 -0500

    Implement basic tests for filter and sorting actions

---
+++ react-redux/src/tests/actions/filters.test.js
@@ -0,0 +1,60 @@
+import {
+   setStartDate, 
+   setEndDate, 
+   setTextFilter, 
+   sortByAmount, 
+   sortByDate 
+  } from '../../actions/filters'
+import moment from 'moment'
+
+test('should generate set text filter action object', () => {
+  const action = setTextFilter('word')
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: 'word'
+  })
+})
+
+test('should generate set text filter action object with default value', () => {
+  const action = setTextFilter()
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  })
+})
+
+test('should generate sort by amount action object v2', () => {
+  expect(sortByAmount()).toEqual({type: 'SORT_BY_AMOUNT'});
+})
+
+test('should generate sort by amount action object', () => {
+  const action = sortByAmount(500);
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  })
+})
+
+test('should generate sort by date action object', () => {
+  const action = sortByDate(500);
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  })
+})
+
+
+test('should generate set start date action object', () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate: moment(0)
+  })
+})
+
+test('should generate set end date action object', () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate: moment(0)
+  })
+})
+
