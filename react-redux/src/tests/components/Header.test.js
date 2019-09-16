b21b4ffa071820eaf3ee7c6383091d7cbdfaa85c
Victor Liu <vliu@ibm.com>
2019-09-16 18:04:58 -0500

    Fix testing with enzyme

---
+++ react-redux/src/tests/components/Header.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import Header from '../../components/Header'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJSON from 'enzyme-to-json';
+Enzyme.configure({adapter: new Adapter()});
+
+test('should render Header', () => {
+  const wrapper = shallow(<Header />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+})
+
+// shallow & full DOM rendering
+// full DOM renders child components
