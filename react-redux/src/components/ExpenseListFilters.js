2c43763462fc421f958c5170f5b978ad6c86bdf2
Victor Liu <victorliu716@gmail.com>
2019-09-15 19:01:45 -0500

    Implement jest sample test cases

--- react-redux/src/components/ExpenseListFilters.js
+++ react-redux/src/components/ExpenseListFilters.js
@@ -35,6 +35,7 @@ class ExpenseListFilters extends React.Component {
           <option value="date" >Date</option>
           <option value = "amount">Amount</option>
         </select>
+        
         <DateRangePicker
           startDate = {this.props.filters.startDate}
           endDate = {this.props.filters.endDate}
