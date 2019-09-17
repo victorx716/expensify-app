b2d665f24ca4e76795f014f434d1ed36606c0d30
Victor Liu <vliu@ibm.com>
2019-09-17 18:30:53 -0500

    Add tests to expense list filters

--- react-redux/src/components/ExpenseListFilters.js
+++ react-redux/src/components/ExpenseListFilters.js
@@ -10,11 +10,23 @@ class ExpenseListFilters extends React.Component {
   };
 
   onDatesChange = ({ startDate, endDate}) => {
-    this.props.dispatch(setStartDate(startDate));
-    this.props.dispatch(setEndDate(endDate));
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
   }
   onFocusChange = (calendarFocused) => {
     this.setState(() => ({ calendarFocused }))
+  }
+
+  onTextChange = (e) => { 
+    props.setTextFilter(e.target.value);
+    }
+  
+  onSortChange = (e) => {
+    if (e.target.value === 'date') {
+      props.sortByDate()
+    } else if (e.target.value === 'amount'){
+      props.sortByAmount()
+    }
   }
   render() {
     return (
@@ -22,17 +34,9 @@ class ExpenseListFilters extends React.Component {
         <input 
           type="text" 
           value = {this.props.filters.text} 
-          onChange={(e) => { 
-            props.dispatch(setTextFilter(e.target.value));
-            }}
+          onChange={this.onTextChange}
         />
-        <select value ={this.props.filters.sortBy} onChange={(e) => {
-          if (e.target.value === 'date') {
-            props.dispatch(sortByDate())
-          } else if (e.target.value === 'amount'){
-            props.dispatch(sortByAmount())
-          }
-          }}>
+        <select value ={this.props.filters.sortBy} onChange={this.onSortChange}>
           <option value="date" >Date</option>
           <option value = "amount">Amount</option>
         </select>
@@ -54,11 +58,15 @@ class ExpenseListFilters extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    filters: state.filters
-  }
-}
+const mapStateToProps = (state) => ({
+  filters: state.filters
+})
 
-// hi 
-export default connect(mapStateToProps)(ExpenseListFilters);
+const mapDispatchToProps = (dispatch) => ({
+  setTextFilter: (text) => dispatch(setTextFilter(text)),
+  sortByDate: () => dispatch(sortByDate()),
+  sortByAmount: () => dispatch(sortByAmount()),
+  setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+  setEndDate : (endDate) => dispatch(setEndDate(endDate))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
---
+++ react-redux/src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {ExpenseListFilters} from '../../components/ExpenseListFilters'
+import { filters, altFilters } from '../fixtures/filters'
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();   
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();  
+  wrapper = shallow(
+  <ExpenseListFilters
+    filters = {filters}
+    setTextFilter = {setTextFilter}
+    sortByDate = {sortByDate}
+    sortByAmount = {sortByAmount}
+    setStartDate={setStartDate}
+    setEndDate={setEndDate}
+  />)
+});
+
+test('should render Expense list filters correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+// test('should render Expense list with alt filters correctly', () => {
+//   wrapper.setProps({
+//     filters: altFilters
+//   })
+//   expect(wrapper).toMatchSnapshot();
+// });
