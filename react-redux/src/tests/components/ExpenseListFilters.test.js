9f9809e80bd2f114717c52a90fefecd5f902cbe6
Victor Liu <vliu@ibm.com>
2019-09-18 10:08:57 -0500

    Finish writing automated test suite

--- react-redux/src/tests/components/ExpenseListFilters.test.js
+++ react-redux/src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {ExpenseListFilters} from '../../components/ExpenseListFilters'
 import { filters, altFilters } from '../fixtures/filters'
+import moment from 'moment'
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -22,9 +23,9 @@ beforeEach(() => {
   />)
 });
 
-test('should render Expense list filters correctly', () => {
-  expect(wrapper).toMatchSnapshot();
-});
+// test('should render Expense list filters correctly', () => {
+//   expect(wrapper).toMatchSnapshot();
+// });
 
 // test('should render Expense list with alt filters correctly', () => {
 //   wrapper.setProps({
@@ -32,3 +33,34 @@ test('should render Expense list filters correctly', () => {
 //   })
 //   expect(wrapper).toMatchSnapshot();
 // });
+
+
+test('should handle text change', () => {
+  const value = 'rent'
+  wrapper.find('input').simulate('change', {
+    target: { value}
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+  const value = 'date'
+  wrapper.find('select').simulate('change', {
+    target: { value}
+  })
+  expect(sortByAmount).toHaveBeenCalled();
+})
+
+test('should handle date changes', () => {
+  const startDate = moment(0).add(4, 'years');
+  const endDate = moment(0).add(8, 'years');
+  wrapper.find('DateRangePicker').prop('onDatesChange')({startDate, endDate});
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+})
+
+test('should handle date focus changes', () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
+})
