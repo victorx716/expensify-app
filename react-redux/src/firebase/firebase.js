fda67c28eb34dfc56abe5b3223c63245033450fa
Victor Liu <vliu@ibm.com>
2019-09-19 23:02:54 -0500

    Include firebase array methods

--- react-redux/src/firebase/firebase.js
+++ react-redux/src/firebase/firebase.js
@@ -14,15 +14,98 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref()
-  .once('value')
-  .then((snapshot) => {
-    const val = snapshot.val();
-    console.log(val);
-  })
-  .catch((e) => {
-    console.log('error fetching data, ', e)
-  })
+// child_removed event
+
+database.ref('expenses').on('child_removed', () => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());  
+})
+
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log(snapshot.key, snapshot.val());  
+})
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((expense) => {
+//       expenses.push({
+//         id: expense.key,
+//         ...expense.val()
+//       })
+//     });
+//     console.log(expenses)
+//   });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+//   snapshot.forEach((expense) => {
+//     expenses.push({
+//       id: expense.key,
+//       ...expense.val()
+//     })
+//   });  
+//   console.log(expenses)
+// })
+
+// database.ref('expenses').push({
+//   description: 'rent',
+//   note: '',
+//   amount: 8888,
+//   createdAt: 39533
+// })
+
+// database.ref('expenses').push({
+//   description: 'boba',
+//   note: '',
+//   amount: 878,
+//   createdAt: 3953343
+// })
+
+// database.ref('expenses').push({
+//   description: 'fewd',
+//   note: '',
+//   amount: 1111838,
+//   createdAt: 3434333
+// })
+
+
+
+// database.ref('notes/-LpBiSwV-gDxikTT9dGj').remove();
+
+// database.ref('notes').push({
+//   title: 'course topics',
+//   body: 'react native, angular'
+// });
+// const notes = [{
+//   id: '12',
+//   body: 'this is my note',
+//   title: 'first note:',
+// }, {
+//   id: '13',
+//   body: 'this is my 2nd note',
+//   title: 'second note:',  
+// }]
+
+// database.ref('notes').set(notes)
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// })
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('error fetching data, ', e)
+//   })
 
 // database.ref().set({
 //   name: 'Victor Liu',
