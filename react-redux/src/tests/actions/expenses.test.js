fc32f6c78bef1c55136f5d32b72635f9622d37ba
Victor Liu <vliu@ibm.com>
2019-09-23 18:06:17 -0500

    Add startEditExpense

--- react-redux/src/tests/actions/expenses.test.js
+++ react-redux/src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense,
   startRemoveExpense,
   setExpenses,
-  startSetExpenses
+  startSetExpenses,
+  startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -135,4 +136,22 @@ test('should fetch the expenses from firebase', (done) => {
     });
     done();
   });
+});
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount: 21045 };
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+    return database.ref(`expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  })
 });
